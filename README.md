deae1a8446764d9ab12c6644f1207e960ad04f48
kimondo88 <kimondo1988@o2.pl>
2021-08-06 20:45:39 +0200

    add canvas image draw func, works v 1.2.0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # discord-kim-bot
+
+canvas has strict dependencies, advisable before using npm i, 
+
+check https://discordjs.guide/popular-topics/canvas.html before using the bot on your server. 
--- main.js
+++ main.js
@@ -2,26 +2,43 @@ const Discord = require("discord.js")
 const client = new Discord.Client();
 const fetch = require("node-fetch");
 const dotenv = require('dotenv').config();
+const Canvas = require('canvas');
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 }); 
 
-client.on("message", msg => {
+client.on("message", async msg => {
   if (msg.content === "ping") {
     msg.reply("pong");
   }
-  if (msg.content[0] === "$"){
-    if(msg.content.startsWith("$pogoda", 0)){
-      const list = msg.content.split(' '); 
-      if(list.length === 3 && list[2].length === 2){
-        const city = list[1];
-        const country = list[2];
-        fetchWeather(city, country).then( data => msg.channel.send(data) );
-      }
-     
+  if(msg.content.startsWith("$pogoda", 0)){
+    const list = msg.content.split(' '); 
+    if(list.length === 3 && list[2].length === 2){
+      const city = list[1];
+      const country = list[2];
+      fetchWeather(city, country).then( data => msg.channel.send(data) );
     }
   }
+
+  if (msg.content === "$obraz"){
+
+    const canvas = Canvas.createCanvas(200, 200);
+    const context = canvas.getContext('2d');
+
+    const pepe = await Canvas.loadImage('./img/pepe.jpg');
+
+    context.drawImage(pepe, 0, 0, canvas.width, canvas.height);
+    context.font = '20px sans-serif';
+	  // Select the style that will be used to fill the text in
+	  context.fillStyle = '#ffffff';
+    context.fillText('THE PEPE', canvas.width / 2.5, canvas.height / 1.8);
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-to-pepe.png');
+
+    msg.channel.send({ content: `Its a pepe bro, ${msg.author}`, files: [attachment] });
+  }
+
 });
 
 
@@ -30,8 +47,9 @@ console.log(process.env.TOKEN);
 
 client.login(process.env.TOKEN);
 
-async function fetchWeather(city, country){
-  return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${process.env.WEATHERTOKEN}`)
+async function fetchWeather(city, country, lang="pl"){
+  return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},
+  ${country}&APPID=${process.env.WEATHERTOKEN}&lang=${lang}&units=metric`)
   .then(res => {
     console.log(`fetching api weather for ${city}, ${country}`); 
     //console.log(res);
