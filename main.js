8919848d26bafa1564303c65886943fde71e51e6
kimondo88 <kimondo1988@o2.pl>
2021-08-06 18:36:11 +0200

    api call func weather work

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
-const client = new Discord.Client()
-const dotenv = require('dotenv').config()
+const client = new Discord.Client();
+const fetch = require("node-fetch");
+const dotenv = require('dotenv').config();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -10,9 +11,35 @@ client.on("message", msg => {
   if (msg.content === "ping") {
     msg.reply("pong");
   }
+  if (msg.content[0] === "$"){
+    if(msg.content.startsWith("$pogoda", 0)){
+      const list = msg.content.split(' '); 
+      if(list.length === 3 && list[2].length === 2){
+        const city = list[1];
+        const country = list[2];
+        fetchWeather(city, country).then( data => msg.channel.send(data) );
+      }
+     
+    }
+  }
 });
+
+
 
 console.log(process.env.TOKEN);
 
 client.login(process.env.TOKEN);
+
+async function fetchWeather(city, country){
+  return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${process.env.WEATHERTOKEN}`)
+  .then(res => {
+    console.log(`fetching api weather for ${city}, ${country}`); 
+    //console.log(res);
+    return res.json(); 
+  })
+  .then(data => {
+    //console.log(data); 
+    return JSON.stringify(data); 
+  })
+}
 
